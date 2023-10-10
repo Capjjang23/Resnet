db16352720b56050c8d2740b021a24d3fbbd94f8
mmihye <smilehye910@naver.com>
2023-10-10 12:29:47 +0900

    최종 코드

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch
 from sklearn.model_selection import KFold
 
 EPOCH = 10
+result_file = "result_resnet34.txt"
 
 # CUDA 초기화
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,7 +29,7 @@ test_dataset = datasets.ImageFolder(root='testImages', transform=data_transforms
 resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet34')
 resnet.fc = nn.Sequential(
     nn.Dropout(p=0.5),  # 드롭아웃 추가
-    nn.Linear(512, 26)  # 출력층의 뉴런 수는 26
+    nn.Linear(512, 10)  # 출력층의 뉴런 수는 10
 )
 
 # L2 정규화 적용할 가중치 파라미터를 모아둘 리스트
@@ -36,7 +37,10 @@ weight_decay_params = []
 bias_params = []
 
 # 정규화 비율 설정
-weight_decay = 0.001
+weight_decay = 0.1
+
+# 학숩률 설정
+learning_rate = 0.1
 
 # 모든 가중치 파라미터를 추출하여 정규화 적용
 for name, param in resnet.named_parameters():
@@ -47,6 +51,12 @@ for name, param in resnet.named_parameters():
 
 # 교차 검증을 위한 KFold 객체 생성
 kfold = KFold(n_splits=5, shuffle=True)
+
+# 각 폴드에 대한 결과를 저장할 리스트 초기화
+fold_results = []
+
+best_accuracy = 0.0  # 최고 정확도를 저장하기 위한 변수
+best_model_weights = None  # 최고 정확도를 달성한 모델 가중치를 저장하기 위한 변수
 
 # 각 폴드에 대해 반복
 for fold, (train_indices, val_indices) in enumerate(kfold.split(train_dataset)):
@@ -65,20 +75,20 @@ for fold, (train_indices, val_indices) in enumerate(kfold.split(train_dataset)):
     optimizer = optim.SGD([
         {'params': weight_decay_params, 'weight_decay': weight_decay},
         {'params': bias_params, 'weight_decay': 0.0}
-    ], lr=0.001, momentum=0.9)
+    ], lr=learning_rate, momentum=0.9)
 
     # 학습률 스케줄러 생성
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
     # 모델 학습
     resnet.to(device)
-    best_accuracy = 0.0  # 최고 정확도를 저장하기 위한 변수
 
     train_losses = []
     val_losses = []
     accuracies = []
 
     for epoch in range(EPOCH):
+        train_loss = 0.0
         running_loss = 0.0
         val_loss = 0.0
         val_correct = 0
@@ -96,10 +106,10 @@ for fold, (train_indices, val_indices) in enumerate(kfold.split(train_dataset)):
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 1500 == 1499:
-                train_loss = running_loss / 100
-                train_losses.append(train_loss)
-                running_loss = 0.0
+            train_loss = running_loss / 100
+
+        train_losses.append(train_loss)
+        running_loss = 0.0
 
         # 학습률 스케줄링
         scheduler.step()
@@ -115,28 +125,56 @@ for fold, (train_indices, val_indices) in enumerate(kfold.split(train_dataset)):
                 val_total += labels.size(0)
                 val_correct += (predicted == labels).sum().item()
 
-        val_loss = loss.item()
-        val_losses.append(val_loss)
-        accuracy = 100 * val_correct / val_total
-        accuracies.append(accuracy)
-        print('[fold: %d, epoch: %d] train loss: %.3f, val loss: %.3f, accuracy: %.2f' % (
-        fold + 1, epoch + 1, train_losses[-1], val_losses[-1], accuracy))
+            val_loss = loss.item()
+            val_losses.append(val_loss)
+            accuracy = 100 * val_correct / val_total
+            accuracies.append(accuracy)
 
-        # 최고 정확도일 때 모델 저장
-        if accuracy > best_accuracy:
-            best_accuracy = accuracy
-            torch.save(resnet.state_dict(), 'resnet34_best.pth')
+            # 현재 검증 정확도가 최고 정확도보다 높은 경우 모델 가중치 저장
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_model_weights = resnet.state_dict()
+
+                torch.save(best_model_weights, 'best_resnet34_weights.pth')
+                print("=== BEST ACCURACY UPDATE===")
 
     # 결과 저장
-    result_file = "result_resnet34.txt"
+    fold_result = {
+        'fold': fold + 1,
+        'train_losses': train_losses,
+        'val_losses': val_losses,
+        'accuracies': accuracies
+    }
+    fold_results.append(fold_result)
+
+    fold = fold_result['fold']
+    train_losses = fold_result['train_losses']
+    val_losses = fold_result['val_losses']
+    accuracies = fold_result['accuracies']
+
+    print('[fold: %d, epoch: %d] trainImages loss: %.3f, val loss: %.3f, accuracy: %.2f' % (
+        fold, epoch + 1, train_losses[epoch], val_losses[epoch], accuracies[epoch]))
+
+# # 훈련 종료 후 최적 모델 가중치 저장
+# torch.save(best_model_weights, 'best_resnet34_weights.pth')
+
+# 교차 검증 완료 후 결과 출력
+for result in fold_results:
+    fold = result['fold']
+    train_losses = result['train_losses']
+    val_losses = result['val_losses']
+    accuracies = result['accuracies']
+
+    # 결과 저장
     with open(result_file, "a") as f:
-        f.write("******** Fold {} ******/**\n".format(fold+1))
         for epoch in range(EPOCH):
-            f.write("Epoch: {}\n".format(epoch + 1))
-            f.write("Train Loss: {:.3f}\n".format(train_losses[epoch]))
-            f.write("Val Loss: {:.3f}\n".format(val_losses[epoch]))
-            f.write("Accuracy: {:.2f}%\n".format(accuracies[epoch]))
-            f.write("----------\n")
+            f.write('[fold: %d, epoch: %d] train loss: %.3f, val loss: %.3f, accuracy: %.2f\n' % (
+                fold, epoch + 1, train_losses[epoch], val_losses[epoch], accuracies[epoch]))
+
+# 평가
+# 저장된 모델 가중치 불러오기
+resnet.load_state_dict(torch.load('best_resnet34_weights.pth'))
+resnet.eval()  # 모델을 평가 모드로 설정
 
 # 테스트 데이터셋에 대한 DataLoader 생성
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=0)
@@ -156,16 +194,6 @@ with torch.no_grad():
 test_accuracy = 100 * correct / total
 print('Accuracy of the network on the testImages images: %.2f %%' % test_accuracy)
 
-# 모델 가중치 저장
-torch.save(resnet.state_dict(), 'resnet34_weights.pth')
-
-# 결과 저장
-# result_file = "result_resnet34.txt"
 with open(result_file, "a") as f:
-    #     for epoch in range(EPOCH):
-    #         f.write("Epoch: {}\n".format(epoch + 1))
-    #         f.write("Train Loss: {:.3f}\n".format(train_losses[epoch]))
-    #         f.write("Val Loss: {:.3f}\n".format(val_losses[epoch]))
-    #         f.write("Accuracy: {:.2f}%\n".format(accuracies[epoch]))
-    #         f.write("----------\n")
     f.write("\nTest Accuracy: {:.2f}%\n".format(test_accuracy))
+
